aa7fffe19735bdd0d56fbc36e498c6bfd21ad39e
krishnaprasad007 <krishnaprasadnalipireddy@gmail.com>
2018-02-19 20:21:10 -0800

    SpringRestJpaActuatorApplication Project Creation
     SpringRestJpaActuatorApplication Project Creation

---
+++ SpringRestJpaActuator/src/main/java/com/kp/main/SpringRestJpaActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.kp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJpaActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestJpaActuatorApplication.class, args);
+	}
+}
